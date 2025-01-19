3968e9bc0e273c6f998ed2e0b474a6aad4ed0965
Neha Agrahari <neha16agrahari@gmail.com>
2025-01-19 22:59:28 +0530

    add files

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+           Scanner sc = new Scanner(System.in);
+
+              System.out.print("Enter the temperature in Celsius: ");
+        double celsius = sc.nextDouble();
+
+    
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+      
+        System.out.println("The temperature in Fahrenheit is: " + fahrenheit);
+
+     
+        sc.close();
+    }
+}
